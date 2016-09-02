ed59856d4e0eb09fcd51b2053418e965bd4dd971
Jason Pembleton <jason@tbkcreatice.com>
2016-09-01 22:26:06 -0400

    prod features dev

--- src/wp-content/themes/reln/views/shortcodes/features-container.php
+++ src/wp-content/themes/reln/views/shortcodes/features-container.php
@@ -1,13 +1,11 @@
 <div class="product-feature-container">
 	<div class="<?php echo $classes; ?>">
-		<div class="col-sm-12">
-			<div class="product-feature-content">
-				<div class="heading-container">
-					<h3><?php echo $heading; ?></h3>
-					<div class="features-container-underline"></div>
-				</div>
-				{content}
-			</div>
+		<div class="product-feature-content">
+			<div class="heading-container">
+				<h3><?php echo $heading; ?></h3>
+				<div class="features-container-underline"></div>
+			</div>				
 		</div>
-	</div>	<!-- .container -->
-</div> <!-- END .container-fluid-->
+	</div>
+</div>
+{content}
--- src/wp-content/themes/reln/views/shortcodes/product-feature.php
+++ src/wp-content/themes/reln/views/shortcodes/product-feature.php
@@ -1,11 +1,13 @@
-<div class="col-xs-12 col-sm-6 col-md-4 col-lg-15 product-feature <?php echo $classes; ?>" >
-	<div class="product-feature-bg">
-		<img class="" src="<?php echo $bg_image; ?>" alt=""/>
-		<div class="product-feature-fg">
-			<img class="" src="<?php echo $product_image; ?>" alt=""/>
-			<h4><?php echo $heading; ?></h4>
-			<span><?php echo $copy; ?></span>
-			<a href="<?php echo $link; ?>">Learn More <span class="icon-arrow-right"></span></a>
+<div class="product-features"> 
+	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-15 product-feature <?php echo $classes; ?>" >
+		<div class="product-feature-bg">
+			<img class="" src="<?php echo $bg_image; ?>" alt=""/>
+			<div class="product-feature-fg">
+				<img class="" src="<?php echo $product_image; ?>" alt=""/>
+				<h4><?php echo $heading; ?></h4>
+				<span><?php echo $copy; ?></span>
+				<a href="<?php echo $link; ?>">Learn More <span class="icon-arrow-right"></span></a>
+			</div>
 		</div>
 	</div>
 </div>
